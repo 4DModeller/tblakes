7324fc440bfff0f8c22693f5dcd71dfad5cd6a22
mnky9800n <johnm.aiken@gmail.com>
2023-12-05 14:15:37 +0100

    Added docstrings

--- utils.py
+++ utils.py
@@ -3,9 +3,13 @@ import numpy as np
 import pandas as pd
 
 def geotiff_to_dataframe(geotiff_path):
-    """
-    Converts a geotiff to a pandas dataframe
-    
+    """converts geotiff file to a pandas dataframe
+
+    Args:
+        geotiff_path (str): location of geotiff
+
+    Returns:
+        pandas.DataFrame: Dataframe with columns x_coord, y_coord, and the variable name which is inferred from the path
     """
     with rasterio.open(geotiff_path) as src:
         data = src.read(1)  # Reading the first band
@@ -30,6 +34,15 @@ def geotiff_to_dataframe(geotiff_path):
         return df
 
 def get_filenames_with_string(directory_path, search_string):
+    """get file names in the directory that match the search string
+
+    Args:
+        directory_path (str): directory data is stored in
+        search_string (str) : variable name to find in the file name
+
+    Returns:
+        list: list of the directory and file names
+    """
     matching_filenames = []
     for filename in os.listdir(directory_path):
         if search_string in filename:
@@ -37,6 +50,15 @@ def get_filenames_with_string(directory_path, search_string):
     return [directory_path + fn for fn in matching_filenames]
 
 def get_dataframe_from_tiffs(years, variable):
+    """maps the geotiff_to_dataframe function to all the data files.
+
+    Args:
+        years (list): a list of years to iterate across
+        variable (str): variable name
+
+    Returns:
+        pandas.DataFrame: pandas dataframe containing all of the data
+    """
     all_dfs = []
     for year in years:
         yeardf = pd.concat(list(map(geotiff_to_dataframe, get_filenames_with_string('EarthEngineData/', variable+'_'+str(year)))))
@@ -44,3 +66,6 @@ def get_dataframe_from_tiffs(years, variable):
         all_dfs.append(yeardf)
         
     return pd.concat(all_dfs)
+
+if __name__=='__main__':
+    print('One day this might be implemented as a CLI but not this day.')
