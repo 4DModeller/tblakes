e3f26e03be800e66640dbaeef71b4e4c69eb1f17
mnky9800n <johnm.aiken@gmail.com>
2023-12-06 14:17:56 +0100

    refactor process

--- process_data.py
+++ process_data.py
@@ -1,23 +1,57 @@
 import utils
+import tqdm
+import pandas as pd
+
+def process_var(var, years):
+    print(f'processing {var}...')
+    return utils.get_data_frame_from_tiffs(years=years, variable=var)
+
+def process_outcome(var, years):
+    # TODO : implement
 
 if __name__=='__main__':
 
-    print('processing temperature...')
-    temperature = utils.get_dataframe_from_tiffs(years=[2012,], variable='Temperature')
-    temperature.to_csv('processed_data/temperature.csv', index=False)
-    del temperature
-    print(f'temperature written to processed_data/temperature.csv')
+    years = [2012,]
+    variables = ['Temperature', 'Precipitation']
+    outcome_variables = ['gsw',]
+    # mem_variables = ['snowcover',]
+    mem_variables = list()
+    total_steps = 2 * (len(variables) + len(mem_variables) + len(outcome_variables))
+    pbar = tqdm(total=total_steps, 'Process Data')
 
-    print('processing precipitation...')
-    precipitation = utils.get_dataframe_from_tiffs(years=[2012,], variable='Precipitation')
-    precipitation.to_csv('processed_data/precipitation.csv', index=False)
-    del precipitation
-    print('temperature written to processed_data/precipitation.csv')
+    processed_vars = {}
+    for var in variables:
+        processed_vars[var] = process_var(var=var, years=years)
+        pbar.update(1)
 
-    print('processing snow cover...')
-    snowcover = utils.get_dataframe_from_tiffs(years=[2012,], variable='SnowCover', memory_issue=True)
-    snowcover.to_csv('processed_data/snowcover.csv', index=False)
-    del snowcover
-    print('snow cover written to processed_data/snowcover.csv')
+    for var in outcome_variables:
+        processed_vars[var] = process_outcome(var=var, years=years)
+        pbar.update(1)
+
+    df = pd.DataFrame()
+    for var in processed_vars.keys():
+        if df.empty == True:
+            df = pd.concat([df, processed_vars[var]])
+        else:
+            df = df.join(processed_vars[var], on=['x_coord', 'y_coord', 'year'])
+        pbar.update(1)
+
+    # print('processing temperature...')
+    # temperature = utils.get_dataframe_from_tiffs(years=[2012,], variable='Temperature')
+    # temperature.to_csv('processed_data/temperature.csv', index=False)
+    # del temperature
+    # print(f'temperature written to processed_data/temperature.csv')
+
+    # print('processing precipitation...')
+    # precipitation = utils.get_dataframe_from_tiffs(years=[2012,], variable='Precipitation')
+    # precipitation.to_csv('processed_data/precipitation.csv', index=False)
+    # del precipitation
+    # print('temperature written to processed_data/precipitation.csv')
+
+    # print('processing snow cover...')
+    # snowcover = utils.get_dataframe_from_tiffs(years=[2012,], variable='SnowCover', memory_issue=True)
+    # snowcover.to_csv('processed_data/snowcover.csv', index=False)
+    # del snowcover
+    # print('snow cover written to processed_data/snowcover.csv')
 
     print('Finished...')
