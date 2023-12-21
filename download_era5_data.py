2b994fb595aff5ee590133276eb0e6b1d8bc890f
mnky9800n <johnm.aiken@gmail.com>
2023-12-21 14:44:34 +0100

    added glacier mass balance data

--- download_era5_data.py
+++ download_era5_data.py
@@ -4,7 +4,8 @@ import numpy as np
 
 c = cdsapi.Client()
 
-years = np.arange(2000, 2022, 1)
+# years = np.arange(2000, 2022, 1)
+years = np.arange(2000, 2012, 1)
 days = [
     '01', '02', '03',
     '04', '05', '06',
@@ -23,7 +24,25 @@ times = '23:00'
 area = [45, 68, 24, 107]
 
 for year in years:
-    year = str(year)
-    c.retrieve('reanalysis-era5-land', {'year':year, 'month':months, 'time':times, 'day':days, 'area':area
-                                       ,'variable':'2m_temperature', 'format':'netcdf.zip'}
-              ,'data/temp/temp-'+year+'netcdf.zip')
+    # year = str(year)
+
+    # temperature data
+    # c.retrieve('reanalysis-era5-land', {'year':year, 'month':months, 'time':times, 'day':days, 'area':area
+    #                                    ,'variable':'2m_temperature', 'format':'netcdf.zip'}
+    #           ,'data/temp/temp-'+year+'netcdf.zip')
+
+    # glacier data
+    c.retrieve(
+        'derived-gridded-glacier-mass-change',
+        {
+            'variable': 'glacier_mass_change',
+            'product_version': 'wgms_fog_2022_09',
+            'format': 'zip',
+            'hydrological_year': [
+                # '2016_17', '2020_21',
+                # f'{year}_{year - 2000 + 1}',
+                # '2000_01',
+                f'{year}_0{year - 2000 + 1}' if year - 2000 < 9 else f'{year}_{year - 2000 + 1}'
+            ],
+        },
+        f'data/glacier/{year}glaciers.zip')
